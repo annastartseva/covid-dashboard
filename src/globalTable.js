0077c171e0e4953dfe2d2ea7c32b1e88afa1c6a2
Anna Startseva <a.v.starceva@gmail.com>
2020-12-18 16:52:23 +0600

    feat: add global case table

--- src/globalTable.js
+++ src/globalTable.js
@@ -1,35 +1,87 @@
-import getSummaryGlobalData from './main';
+import { state, setDataToAllElement, countPopulationTotal } from './main';
+
+// const stateData = state;
 
 // Table Global Case
 const buttonSummaryAllPeriod = document.querySelector('.button__all-period');
 const buttonSummaryToday = document.querySelector('.button__last-day');
-// const buttonSummaryAbs = document.querySelector('.button__abs button');
-// const buttonSummaryPerPopulation = document.querySelector('.button__per-population');
+const buttonSummaryAbs = document.querySelector('.button__abs');
+const buttonSummaryPerPopulation = document.querySelector('.button__per-population');
 const totalConfirmed = document.querySelector('#total_confirm');
 const totalRecover = document.querySelector('#total_recover');
 const totalDeaths = document.querySelector('#total_deaths');
 
-const getDataForGlobalTable = (data) => {
-  totalConfirmed.innerHTML = data.Global.TotalConfirmed.toLocaleString();
-  totalRecover.innerHTML = data.Global.TotalRecovered.toLocaleString();
-  totalDeaths.innerHTML = data.Global.TotalDeaths.toLocaleString();
+const setDataToDomElement = (confirmed, recover, deaths) => {
+  totalConfirmed.innerHTML = confirmed.toLocaleString();
+  totalRecover.innerHTML = recover.toLocaleString();
+  totalDeaths.innerHTML = deaths.toLocaleString();
+};
+
+const setDataAllPeriodForGlobalTable = (data) => {
+  let totalConfirmedData = data.Global.TotalConfirmed;
+  let totalRecoverData = data.Global.TotalRecovered;
+  let totalDeathsData = data.Global.TotalDeaths;
+  if (state.absValue === false) {
+    totalConfirmedData = Math.round(totalConfirmedData / (state.population / 100000));
+    totalRecoverData = Math.round(totalRecoverData / (state.population / 100000));
+    totalDeathsData = Math.round(totalDeathsData / (state.population / 100000));
+  }
+  setDataToDomElement(totalConfirmedData, totalRecoverData, totalDeathsData);
+};
+
+const setDataTodayForGlobalTable = (data) => {
+  let todayConfirmedData = data.Global.NewConfirmed;
+  let todayRecoverData = data.Global.NewRecovered;
+  let todayDeathsData = data.Global.NewDeaths;
+  if (state.absValue === false) {
+    todayConfirmedData = Math.round(todayConfirmedData / (state.population / 100000));
+    todayRecoverData = Math.round(todayRecoverData / (state.population / 100000));
+    todayDeathsData = Math.round(todayDeathsData / (state.population / 100000));
+  }
+  setDataToDomElement(todayConfirmedData, todayRecoverData, todayDeathsData);
 };
 
 // Listener
 const initializationButtonListener = () => {
   buttonSummaryAllPeriod.addEventListener('click', () => {
+    console.log('state ', state);
+    // console.log('stateData.allPeriod ', state.hasOwnProperty(allPeriod));
     buttonSummaryAllPeriod.classList.add('select');
     buttonSummaryToday.classList.remove('select');
-    getSummaryGlobalData();
+    state.allPeriod = true;
+    console.log('stateData.allPeriod ', state.allPeriod);
+    setDataToAllElement();
   });
 
   buttonSummaryToday.addEventListener('click', () => {
+    console.log('state ', state);
+    console.log('stateData.allPeriod ', state.allPeriod);
     buttonSummaryAllPeriod.classList.remove('select');
     buttonSummaryToday.classList.add('select');
-    getSummaryGlobalData();
+    state.allPeriod = false;
+    console.log('stateData.allPeriod ', state.allPeriod);
+    setDataToAllElement();
+  });
+
+  buttonSummaryAbs.addEventListener('click', () => {
+    buttonSummaryPerPopulation.classList.remove('select');
+    buttonSummaryAbs.classList.add('select');
+    state.absValue = true;
+    console.log('stateData.allPeriod ', state.allPeriod);
+    countPopulationTotal();
+    setDataToAllElement();
+  });
+
+  buttonSummaryPerPopulation.addEventListener('click', () => {
+    buttonSummaryAbs.classList.remove('select');
+    buttonSummaryPerPopulation.classList.add('select');
+    state.absValue = false;
+    console.log('stateData.allPeriod ', state.allPeriod);
+    countPopulationTotal();
+    setDataToAllElement();
   });
 };
 
 initializationButtonListener();
 
-export default getDataForGlobalTable;
+export { setDataAllPeriodForGlobalTable, setDataTodayForGlobalTable };
--- src/main.js
+++ src/main.js
@@ -1,33 +1,63 @@
-import getDataForGlobalTable from './globalTable';
+import { setDataAllPeriodForGlobalTable, setDataTodayForGlobalTable } from './globalTable';
 
 // const url
 const urlSummary = 'https://api.covid19api.com/summary';
 
-// data from API
-let data = '';
+const state = {
+  dataCovid: null, // data from API
+  dataCountryInfo: null, // flag, population
+  allPeriod: true,
+  absValue: true,
+  allWorld: true,
+  countryId: null,
+  population: null, // all word or select country
+};
 
-let setDataToTableGlobal = '';
+// setDataToTableGlobal rename 
+const setDataToAllElement = () => {
+  console.log('function setDataToAllElement');
+  console.log('state ', state);
+  if (state.allPeriod === true) {
+    setDataAllPeriodForGlobalTable(state.dataCovid);
+  } else if (state.allPeriod === false) {
+    setDataTodayForGlobalTable(state.dataCovid);
+  }
+};
+
+const countPopulationTotal = () => {
+  if (state.allWorld === true) {
+    const dataCountryInfo = state.dataCountryInfo;
+    const worldPopulation = dataCountryInfo.reduce((acc, item) => acc + Number(item.population), 0);
+    console.log('worldPopulation ', worldPopulation);
+    state.population = worldPopulation;
+  }
+};
+
+async function getDataByCountry() {
+  console.log('function getDataByCountry');
+  const countriesDataInJSON = await fetch('https://restcountries.eu/rest/v2/all?fields=name;population;flag');
+  state.dataCountryInfo = await countriesDataInJSON.json();
+  console.log('state.dataCountryInfo: ', state.dataCountryInfo);
+}
 
 async function getSummaryGlobalData() {
   console.log('function getSummaryGlobalData');
   const url = urlSummary;
   const res = await fetch(url);
-  data = await res.json();
-  console.log('TotalConfirmed: ', data.Global.TotalConfirmed);
-  console.log('TotalDeaths: ', data.Global.TotalDeaths);
-  console.log('TotalRecovered: ', data.Global.TotalRecovered);
-  setDataToTableGlobal();
+  state.dataCovid = await res.json();
+  console.log('state.dataCovid: ', state.dataCovid);
+  console.log('TotalConfirmed main: ', state.dataCovid.Global.TotalConfirmed);
+  console.log('TotalDeaths: ', state.dataCovid.Global.TotalDeaths);
+  console.log('TotalRecovered: ', state.dataCovid.Global.TotalRecovered);
+  setDataToAllElement();
 }
 
 // first page load
 const firstPageLoad = () => {
+  getDataByCountry();
   getSummaryGlobalData();
-};
-
-setDataToTableGlobal = () => {
-  getDataForGlobalTable(data);
 };
 
 firstPageLoad();
 
-export default getSummaryGlobalData;
+export { getSummaryGlobalData, state, setDataToAllElement, countPopulationTotal };
